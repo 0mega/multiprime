cc564ee5d76ff3137a0774d1b1f5ea971d31e9d1
Oleksandr Kruk <kruk.oleksandr@gmail.com>
2015-08-18 00:35:25 +0100

    Use TextTable to print multiplication table of primes; Update tests

--- lib/multiprime/table_generator.rb
+++ lib/multiprime/table_generator.rb
@@ -1,10 +1,26 @@
 require 'multiprime/prime_gen'
+require 'text-table'
 
 module Multiprime
 	class TableGenerator
 	  def self.print_table prime_count
 	    primes = PrimeGen.instance.get_consecutive_primes prime_count
-	    puts "#{primes}"
+
+			table = Text::Table.new
+			table.head = primes.unshift("")
+			(primes.length-1).times do |idx|
+				row = []
+				primes.each_with_index do |p, j|
+					if (p == "")
+						row << primes[idx+1]
+					else
+						row << row[0] * p
+					end
+
+				end
+				table.rows << row
+			end
+			puts table
 	  end
 	end
 end
